a8d25b6b1883a55ada050ca4cf723a558686fd5a
alek <wait82985@gmail.com>
2023-08-03 13:13:36 +0600

    problem solved

--- src/main/java/Solution.java
+++ src/main/java/Solution.java
@@ -1,6 +1,23 @@
+import java.util.Arrays;
+
 public class Solution {
     public int maximumBeauty(int[] nums, int k) {
-        //TODO
-        return 0;
+        Arrays.sort(nums);
+        int maxLength = 1;
+
+        int left = 0;
+        int right = 0;
+
+        while (right < nums.length){
+            if(nums[right] - nums[left] <= k * 2){
+                maxLength = Math.max(right - left + 1, maxLength);
+                right++;
+            }else {
+                left++;
+            }
+        }
+
+        return maxLength;
     }
+
 }
